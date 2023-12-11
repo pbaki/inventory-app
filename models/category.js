b0ae2b71592494cead0c2c5a2dca40af079e4549
pbaki <pbaki999@gmail.com>
2023-12-11 17:57:42 +0100

    models

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 100 },
+  items: { type: Schema.Types.ObjectId, ref: "Item" },
+});
+
+CategorySchema.virtual("url").get(function () {
+  return `/catalog/categories/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
