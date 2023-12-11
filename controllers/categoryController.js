9abc1895ad72521f4803a822ec17fe94971e8c36
pbaki <pbaki999@gmail.com>
2023-12-11 21:09:34 +0100

    read views

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -16,10 +16,20 @@ exports.index = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented yet");
+  const Allcategories = await Category.find().sort({ name: 1 });
+
+  res.render("category_list", {
+    title: "All Categories",
+    categories: Allcategories,
+  });
 });
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented yet");
+  const category = await Category.findById(req.params.id).populate("items");
+
+  res.render("category_details", {
+    title: "Category details",
+    category: category,
+  });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,13 +1,23 @@
-const item = require("../models/item");
+const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented yet");
+  const Allitems = await Item.find().sort({ name: 1 });
+
+  res.render("item_list", {
+    title: "All Items",
+    items: Allitems,
+  });
 });
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented yet");
+  const item = await Item.findById(req.params.id).populate("category");
+
+  res.render("item_details", {
+    title: "Item details",
+    item: item,
+  });
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
